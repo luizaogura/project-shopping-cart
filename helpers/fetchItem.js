2023a55e41b778ca38a9eb65837149bab4e900ab
Luiza Ogura <luizapogura@gmail.com>
2022-10-11 22:49:35 -0300

    Implementando a função fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,10 @@
-const fetchItem = () => {
+const fetchItem = (ItemID) => {
   // seu código aqui
+  const url = fetch(`https://api.mercadolibre.com/items/${ItemID}`)
+  .then((response) => response.json())
+  .catch((error) => error);
+
+  return url;
 };
 
 if (typeof module !== 'undefined') {
