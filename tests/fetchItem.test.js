c5619c3e138aba6a2ade976d92d468c0ad0496e5
Luiza Ogura <luizapogura@gmail.com>
2022-10-11 22:40:29 -0300

    Implementando os testes da função fetchItem

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -4,5 +4,23 @@ const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toBe('function');
+  });
+  it('Execute a função fetchItem com o argumento do item "MLB1615760527" e teste se fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Teste se com o argumento do item "MLB1615760527" a função fetch utiliza o endpoint', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+  it('Teste se o retorno com o argumento computador é uma estrutura de dados igual ao objeto item', async () => {
+    const retorno = await fetchItem('MLB1615760527');
+    expect(retorno).toEqual(item);
+  });
+  it('Teste se, ao chamar a função fetchItem sem argumento, retorna um erro com a mensagem', async () => {
+    const retorno = await fetchItem();
+    expect(retorno).toEqual(new Error('You must provide an url'));
+  });
 });
