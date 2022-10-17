8487b564fb0cf050a7a039a3993fea94f16e34d1
Luiza Ogura <luizapogura@gmail.com>
2022-10-17 19:37:21 -0300

    consertando teste de getSavedCartItens

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -6,11 +6,11 @@ localStorageSimulator('getItem');
 describe('4 - Teste a função getSavedCartItems', () => {
   // implemente seus testes aqui
   it('Teste se, ao executar saveCartItems o método localStorage.setItem é chamado', async () => {
-    await saveCartItems();
+    await getSaveCartItems();
     expect(localStorage.getItem).toHaveBeenCalled();
   });
   it('Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o cartItems como parâmetro', async () => {
-    await saveCartItems();
+    await getSaveCartItems();
     expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
   });
 });
