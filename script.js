a98efa3a7b3e449b9dd133fc16c7cf860df7008b
Luiza Ogura <luizapogura@gmail.com>
2022-10-17 23:01:51 -0300

    Voltando comentarios JSdoc

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const getItems = document.querySelector('.cart__items');
 
- // Função responsável por criar e retornar o elemento de imagem do produto.
+ /**
+ * Função responsável por criar e retornar o elemento de imagem do produto.
+ * @param {string} imageSource - URL da imagem.
+ * @returns {Element} Elemento de imagem do produto.
+ */
  const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -8,7 +12,13 @@ const getItems = document.querySelector('.cart__items');
   return img;
 };
 
-// Função responsável por criar e retornar qualquer elemento.
+/**
+ * Função responsável por criar e retornar qualquer elemento.
+ * @param {string} element - Nome do elemento a ser criado.
+ * @param {string} className - Classe do elemento.
+ * @param {string} innerText - Texto do elemento.
+ * @returns {Element} Elemento criado.
+ */
 const createCustomElement = (element, className, innerText) => {
   const e = document.createElement(element);
   e.className = className;
@@ -30,6 +40,14 @@ const pageLoaded = () =>
     .querySelector('.items')
     .removeChild(document.querySelector('.loading'));
 
+/**
+ * Função responsável por criar e retornar o elemento do produto.
+ * @param {Object} product - Objeto do produto. 
+ * @param {string} product.id - ID do produto.
+ * @param {string} product.title - Título do produto.
+ * @param {string} product.thumbnail - URL da imagem do produto.
+ * @returns {Element} Elemento de produto.
+ */
 const createProductItemElement = ({ id, title, thumbnail }) => {
   const section = document.createElement('section');
   section.className = 'item';
@@ -47,11 +65,21 @@ const cartItemClickListener = (e) => {
   e.target.remove();
 };
 
-// Função responsável por criar e retornar o elemento do produto.
-// Função que recupera o ID do produto passado como parâmetro.
+/**
+ * Função que recupera o ID do produto passado como parâmetro.
+ * @param {Element} product - Elemento do produto.
+ * @returns {string} ID do produto.
+ */
 const getIdFromProductItem = (product) => product.querySelector('.item_id').innerText;
 
-// Função responsável por criar e retornar um item do carrinho.
+/**
+ * Função responsável por criar e retornar um item do carrinho.
+ * @param {Object} product - Objeto do produto.
+ * @param {string} product.id - ID do produto.
+ * @param {string} product.title - Título do produto.
+ * @param {string} product.price - Preço do produto.
+ * @returns {Element} Elemento de um item do carrinho.
+ */
 const createCartItemElement = ({ id, title, price }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
