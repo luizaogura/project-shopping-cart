2d9aa8d868e60d0b32bd50efdef73e72673ad922
Luiza Ogura <luizapogura@gmail.com>
2022-10-17 22:48:22 -0300

    Alterando id de fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,5 @@
-const fetchItem = async (itemId) => {
-  const responseFetch = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+const fetchItem = async (ItemID) => {
+  const responseFetch = await fetch(`https://api.mercadolibre.com/items/${ItemID}`);
   const data = await responseFetch.json();
   return data;
  };
